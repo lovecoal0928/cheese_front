230ab925d3aefcc3da4b8e5b41eccd26ee9d208e
NewNomad <tosiki1997@gmail.com>
2023-05-02 01:59:12 +0900

    fix: eslint error

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import React from 'react';
 
 
 export default function App() {
