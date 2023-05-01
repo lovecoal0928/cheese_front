49167b0e6b5b65541a377060ddc0a0d0a87b0ea0
NewNomad <tosiki1997@gmail.com>
2023-05-02 01:33:05 +0900

    feat: AppNavigator.tsx

---
+++ src/navigations/AppNavigator.tsx
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+
+export const AppNavigator = () => {
+ const user=1
+ 
+    return (
+    <NavigationContainer>
+        {/* {user ? <AppNavigator /> : <AuthNavigator />} */}
+    </NavigationContainer>
+
+  )
+}
