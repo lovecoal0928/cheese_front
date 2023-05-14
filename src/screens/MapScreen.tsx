6e151bb6f1490f5aea8023e7ca0e647bcd14589c
NewNomad <tosiki1997@gmail.com>
2023-05-14 18:30:51 +0900

    ‼️ BREAKING: react-native-geolocation-serviceの削除

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
-import Geolocation from 'react-native-geolocation-service';
 
 type ILocation = {
     latitude: number;
@@ -10,17 +9,7 @@ type ILocation = {
 
 export const MapScreen = () => {
 
-    const [location, setLocation] = useState<ILocation | undefined>(undefined);
 
-    useEffect(() => {
-        Geolocation.getCurrentPosition(info => {
-            setLocation({
-                latitude: info.coords.latitude,
-                longitude: info.coords.longitude,
-            });
-        }, error => console.log(error), { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
-        );
-    }, []);
 
 
     return (
