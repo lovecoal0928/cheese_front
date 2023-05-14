f478a2ca5f0cc685cb2680891c862553623268f5
NewNomad <tosiki1997@gmail.com>
2023-05-14 19:01:39 +0900

    👌 IMPROVE: themeのカラーパレット変更

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
-import { Text } from 'react-native-paper';
+import { Button, SegmentedButtons, Text } from 'react-native-paper';
 
 export const MapScreen = () => {
 
     const [location, setLocation] = useState<Location.LocationObjectCoords | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    // 現在選択中のボタンの値を管理する
+    const [selectedButton, setSelectedButton] = useState<string>('');
 
     useEffect(() => {
         (async () => {
@@ -25,6 +28,17 @@ export const MapScreen = () => {
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
+            <View>
+                <SegmentedButtons
+                    value={selectedButton}
+                    onValueChange={(value) => setSelectedButton(value)}
+                    buttons={[
+                        { label: 'すべて', value: 'all' },
+                        { label: 'おすすめ', value: 'recommend' },
+                    ]}
+
+                />
+            </View>
             {
                 // 現在位置が取得できない場合はエラーを表示する
                 location !== null ?
