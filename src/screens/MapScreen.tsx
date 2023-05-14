5b296ba5d2f6e73c5d5993df83f1506866e913c8
NewNomad <tosiki1997@gmail.com>
2023-05-14 18:06:15 +0900

    👌 IMPROVE: useStateの管理

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 
 export const MapScreen = () => {
+    // 現在地を管理する
+    const [currentLocation, setCurrentLocation] = useState({
+        latitude: 0,
+        longitude: 0,
+    });
+
+
+
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <MapView style={styles.mapview} >
+            <MapView
+                style={styles.mapview}
+                initialRegion={{ latitude: 35.681236, longitude: 139.767125, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
+            >
 
             </MapView>
         </SafeAreaView>
