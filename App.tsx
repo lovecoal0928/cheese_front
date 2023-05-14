9a333ead6fea3314a02977a2aa58ac62b39d6551
NewNomad <tosiki1997@gmail.com>
2023-05-14 19:01:59 +0900

    🐛 FIX: App.tsx

--- App.tsx
+++ App.tsx
@@ -13,8 +13,10 @@ export default function App() {
             primary: '#333',
             onPrimary: '#fff',
             primaryContainer: '#000',
+            onPrimaryContainer: '#fff',
             secondary: '#FFEDAF',
-            secondaryContainer: '#fff',
+            secondaryContainer: '#333',
+            onSecondaryContainer: '#fff',
             background: '#f4f4f4',
             surfaceVariant: '#fff'
 
