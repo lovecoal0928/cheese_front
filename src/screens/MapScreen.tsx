c54fa8bfbe8663654bd256ac202d2a2978905d48
NewNomad <tosiki1997@gmail.com>
2023-05-14 18:01:46 +0900

    👌 IMPROVE: ViewをSafeAreaViewに変更

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 
 export const MapScreen = () => {
     return (
-        <View style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }}>
             <MapView style={styles.mapview} >
 
             </MapView>
-        </View>
+        </SafeAreaView>
     );
 };
 
