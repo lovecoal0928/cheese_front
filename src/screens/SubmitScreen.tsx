1116484c087444dda5015ce0b10948fdcd3c8b0a
NewNomad <tosiki1997@gmail.com>
2023-05-14 17:42:21 +0900

    [sparkles]feat: 新規投稿画面

--- src/screens/SubmitScreen.tsx
+++ src/screens/SubmitScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Button, Text, TextInput } from 'react-native-paper';
 
 export const SubmitScreen = () => {
@@ -10,18 +10,34 @@ export const SubmitScreen = () => {
             />
             <TextInput
                 label={'場所'}
+                right={<TextInput.Icon icon="map-marker" onPress={() => console.log('aa')
+                } />}
             />
             <TextInput
                 label={'コメント'}
                 multiline={true}
                 numberOfLines={10}
-                style={{ height: 120 }}
+                style={styles.textarea}
             />
             <Button
                 mode="contained"
+                icon={'camera'}
+                style={styles.submitBtn}
                 onPress={() => console.log('Pressed')}
             >
                 写真を追加</Button>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    textarea: {
+        height: 200,
+    },
+    submitBtn: {
+        marginTop: 64,
+        width: 200,
+        borderRadius: 500,
+        alignSelf: 'center',
+    }
+});
