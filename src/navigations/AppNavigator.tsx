3570348130633145dc6203ed0169e6847a890585
yudai2929 <yudai201729@icloud.com>
2023-06-07 00:43:51 +0900

    🗑delete: 不要な処理を削除

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -1,22 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { AuthScreen } from '../screens/AuthScreen';
 import { MainStackNavigator } from './MainStackNavigator';
-import { useSignIn } from '../hooks/domain/auth/useSignIn';
-import { User } from 'firebase/auth';
-import { useSignUp } from '../hooks/domain/auth/useSignUp';
 
 export const AppNavigator = () => {
-    const { mutate: signIn } = useSignIn();
-    const { mutate: signUp } = useSignUp();
-    const [user, setUser] = useState<User>();
-    useEffect(() => {
-        if (user) return;
-        console.log('login render');
-        const email = 'hoge@hoge.com';
-        const password = 'hogehoge';
-        signIn({ email, password }, { onSuccess: (data) => setUser(data) });
-    }, []);
+    const user = 1;
 
     return (
         <NavigationContainer>
