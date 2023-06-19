f2b64e8109a55fd7a5fe7d13972d5f89664c46a4
NewNomad <tosiki1997@gmail.com>
2023-06-19 16:00:02 +0900

    📦 NEW: SignUpScreen

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ActivityIndicator, StyleSheet } from 'react-native';
-import { Text, TextInput } from 'react-native-paper';
+import { Button, Text, TextInput } from 'react-native-paper';
 
-export const SignInScreen = () => {
+export const SignUpScreen = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const handleEmailChange = (email: string) => setEmail(email);
+    const handlePasswordChange = (password: string) => setPassword(password);
+
     return (
         <SafeAreaProvider style={styles.container}>
-            <TextInput label="Email" value="" />
-            <TextInput label="Password" value="" />
+            <Text style={styles.header}>ログイン</Text>
+            <TextInput
+                label="Email"
+                value={email}
+                onChangeText={handleEmailChange}
+                style={styles.text}
+            />
+            <TextInput
+                label="Password"
+                value={password}
+                onChangeText={handlePasswordChange}
+                secureTextEntry
+                style={styles.text}
+            />
+            <Button
+                mode="contained"
+                onPress={() => console.log('Pressed')}
+                style={styles.button}
+            >
+                ログイン
+            </Button>
+            <Button
+                mode="outlined"
+                onPress={() => console.log('Pressed')}
+                style={styles.button2}
+            >
+                新規登録
+            </Button>
         </SafeAreaProvider>
     );
 };
@@ -16,12 +48,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        padding: 16,
+
         justifyContent: 'center',
     },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
     text: {
-        marginTop: 16,
-        fontSize: 12,
-        color: '#888',
+        marginTop: 20,
+    },
+    button: {
+        marginTop: 120,
+    },
+    button2: {
+        marginTop: 20,
     },
 });
